e6a57328e08794430a83aea23693f074247ed1ab
Kurt McKee <contactme@kurtmckee.org>
2023-01-18 12:38:17 -0600

    Establish dependencies among the tox environments
    This allows tox to run the environments in parallel.
    This is a big performance gain:
    
    Serial tox runs (with all environments already built) take ~20 seconds.
    Parallel tox runs now take only ~5 seconds to complete.
    
    This requires a change to the `whattimeisitnow` example app.
    Previously it was using an on-disk file, which led to conflicts.
    Now, it's using an in-memory dictionary as its "database".

--- examples/whattimeisitrightnow/app/database.py
+++ examples/whattimeisitrightnow/app/database.py
@@ -1,25 +1,19 @@
-import dbm
-import os
 import pickle
 
 
 class DummyDatabase:
-    filename = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "whattimeisitrightnow.example.db"
-    )
+    def __init__(self):
+        self.db = {}
 
     def persist(self, key, value):
-        with dbm.open(self.filename, "c") as db:
-            db[key] = pickle.dumps(value)
+        self.db[key] = pickle.dumps(value)
 
     def query(self, key):
-        with dbm.open(self.filename, "c") as db:
-            val = db.get(key, pickle.dumps(None))
+        val = self.db.get(key, pickle.dumps(None))
         return pickle.loads(val)
 
     def delete(self, key):
-        with dbm.open(self.filename, "c") as db:
-            del db[key]
+        del self.db[key]
 
 
 db = DummyDatabase()
